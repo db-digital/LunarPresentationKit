30766881961ac1090e36e9ef8eb6adf26c38f2a3
Rajat Agrawal <agrawal.rajat.89@gmail.com>
2019-09-07 12:15:04 +0530

    Add template files for test app project

---
+++ LunarPresentationKit/LNPresentationKitTestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LNPresentationKitTestApp
+//
+//  Created by Rajat Agrawal on 9/7/19.
+//  Copyright © 2019 Rajat Agrawal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
