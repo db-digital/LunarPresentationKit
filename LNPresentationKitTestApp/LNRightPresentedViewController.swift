955b1f6625c4cb833c49959f02cf7fd4b7675320
Rajat Agrawal <agrawal.rajat.89@gmail.com>
2019-09-15 07:24:13 +0530

    Move dismissal pan gesture from presented view controller to presentation view controller

--- LNPresentationKitTestApp/LNRightPresentedViewController.swift
+++ LNPresentationKitTestApp/LNRightPresentedViewController.swift
@@ -16,39 +16,7 @@ class LNRightPresentedViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        addPanGesture()
         view.backgroundColor = .orange
-        if let lnPresentationController = presentationController as? LNPresentationController {
-            lnPresentationController.dismissalViewPanGesture = panGesture
-        }
-
         // Do any additional setup after loading the view.
     }
-    
-    func addPanGesture() {
-        panGesture.edges = .left
-        panGesture.addTarget(self, action:#selector(self.handlePan(gesture:)))
-        view.addGestureRecognizer(panGesture)
-    }
-    
-    @objc func handlePan(gesture : UIScreenEdgePanGestureRecognizer) {
-        if (gesture.state == .began && (!isBeingDismissed) && !isBeingPresented) {
-            DDLogDebug("dismissal pan gesture : Dismissing presented view controller")
-            dismiss(animated: true, completion: nil)
-        } else {
-            DDLogDebug("dismissal pan gesture : Falling through")
-        }
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ LunarPresentationKit/Classes/LNSideSheetPresentationController.swift
@@ -0,0 +1,13 @@
+//
+//  LNSideSheetPresentationController.swift
+//  LunarPresentationKit
+//
+//  Created by Rajat Agrawal on 9/14/19.
+//  Copyright © 2019 Rajat Agrawal. All rights reserved.
+//
+
+import UIKit
+
+class LNSideSheetPresentationController: LNPresentationController {
+
+}
